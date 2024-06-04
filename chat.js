203da8d0af6166c565904719707f9236258ab8a2
jungujeong <devjungu@gmail.com>
2024-06-05 04:37:45 +0900

    6.5일 프론트 회의전

---
+++ chat.js
@@ -0,0 +1,512 @@
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+const mysql = require('mysql2');
+const fs = require('fs');
+const path = require('path');
+const session = require('express-session');
+
+const sessionParser = session({
+    secret: 'your_secret_key',
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: false }
+});
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+
+const pool = mysql.createPool({
+    host: 'localhost',
+    user: 'root',
+    password: '1234',
+    database: 'chat_db',
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
+});
+
+let markers = {}; // 마커 정보를 저장할 객체
+let userMarkers = {}; // 사용자별 마커 ID 저장
+let roomUserLocations = {}; // 방별로 사용자 위치를 저장하는 객체
+let roomUsers = {}; // 방별 사용자 목록
+let bannedUsers = {}; // 방별 강퇴된 사용자 목록
+const roomUserCounts = {}; // 방별 유저 수를 저장하는 객체
+
+
+
+app.use(sessionParser);
+app.use('/images', express.static(path.join(__dirname, 'images')));
+
+function calculateCenter(locations) {
+    let latSum = 0;
+    let lngSum = 0;
+    locations.forEach(loc => {
+        latSum += loc.latitude;
+        lngSum += loc.longitude;
+    });
+    return {
+        latitude: latSum / locations.length,
+        longitude: lngSum / locations.length
+    };
+}
+
+function loadMarkersFromDB() {
+    pool.query('SELECT * FROM markers', (err, results) => {
+        if (err) {
+            console.error('Error loading markers from MySQL:', err);
+            return;
+        }
+        results.forEach(marker => {
+            markers[marker.id] = marker;
+
+            // 유저 수 정보를 포함시켜서 클라이언트로 전송
+            const userCount = roomUsers[marker.id] ? roomUsers[marker.id].length : 0;
+            const maxNumber = marker.max_number || 0;
+
+            io.emit('newMarker', {
+                ...marker,
+                userCount: userCount,
+                maxNumber: maxNumber
+            });
+        });
+    });
+}
+io.use((socket, next) => {
+    sessionParser(socket.request, {}, next);
+});
+
+io.on('connection', (socket) => {
+
+    const req = socket.request;
+    if (req.session.username) {
+        socket.username = req.session.username;
+        console.log(`Session user: ${req.session.username}`);
+    }
+
+    // 기존 마커 정보를 클라이언트에 전송
+    Object.values(markers).forEach(marker => {
+        const userCount = roomUsers[marker.id] ? roomUsers[marker.id].length : 0;
+        const maxNumber = marker.max_number || 0;
+
+        socket.emit('newMarker', {
+            ...marker,
+            userCount: userCount,
+            maxNumber: maxNumber
+        });
+    });
+    socket.on('joinRoom', (roomId) => {
+        if (bannedUsers[roomId] && bannedUsers[roomId].includes(socket.username)) {
+            socket.emit('banned', { success: false, message: 'You are banned from this room.' });
+            return;
+        }
+
+        const currentUsersCount = roomUsers[roomId] ? roomUsers[roomId].length : 0;
+        const maxNumber = markers[roomId] ? markers[roomId].max_number : 0;
+
+        if (currentUsersCount >= maxNumber) {
+            socket.emit('roomFull', { success: false, message: 'Room is full' });
+            return;
+        }
+
+        socket.join(roomId);
+        socket.room = roomId;
+        console.log(`${socket.username} joined room ${roomId}`);
+
+        if (!roomUserLocations[roomId]) {
+            roomUserLocations[roomId] = [];
+        }
+        if (!roomUsers[roomId]) {
+            roomUsers[roomId] = [];
+        }
+
+        if (!roomUsers[roomId].includes(socket.username)) {
+            roomUsers[roomId].push(socket.username);
+        }
+
+        roomUserCounts[roomId] = {
+            userCount: roomUsers[roomId].length,
+            maxNumber: markers[roomId] ? markers[roomId].max_number : 0
+        };
+
+        const isAdmin = markers[roomId].created_by === socket.username;
+        socket.emit('joinedRoom', roomId, markers[roomId], isAdmin);
+
+        io.to(roomId).emit('updateUserLocations', {
+            userLocations: roomUserLocations[roomId],
+            center: calculateCenter(roomUserLocations[roomId]),
+            users: roomUsers[roomId]
+        });
+
+        io.emit('updateUserCount', {
+            roomId: roomId,
+            userCount: roomUserCounts[roomId].userCount,
+            maxNumber: roomUserCounts[roomId].maxNumber
+        });
+    });
+
+    socket.on('updateLocation', (data) => {
+        const { latitude, longitude, roomId } = data;
+        const username = socket.username;
+
+        const userLocation = { username, latitude, longitude };
+        let userLocations = roomUserLocations[roomId] || [];
+
+        const index = userLocations.findIndex(loc => loc.username === username);
+        if (index !== -1) {
+            userLocations[index] = userLocation;
+        } else {
+            userLocations.push(userLocation);
+        }
+
+        roomUserLocations[roomId] = userLocations;
+
+        const center = calculateCenter(userLocations);
+
+        io.to(roomId).emit('updateUserLocations', {
+            userLocations: userLocations,
+            center: center,
+            users: roomUsers[roomId]
+        });
+    });
+
+    socket.on('createMarker', (markerData) => {
+        if (userMarkers[socket.username]) {
+            socket.emit('markerExists', { success: false, message: 'Marker already exists' });
+            return;
+        }
+    
+        markerData.admin = socket.id;
+        const roomId = markerData.id;
+        socket.join(roomId);
+        socket.room = roomId;
+        console.log(`${socket.username} created and joined room ${roomId}`);
+    
+        markers[roomId] = markerData; // Store the full marker data
+        userMarkers[socket.username] = roomId;
+        io.emit('newMarker', markerData);
+    
+        pool.query('INSERT INTO markers (id, title, created_by, context, latitude, longitude, max_number, type, image) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)', 
+            [markerData.id, markerData.title, markerData.created_by, markerData.context, markerData.latitude, markerData.longitude, markerData.max_number, markerData.type, markerData.image], 
+            (err) => {
+                if (err) {
+                    console.error('Error saving marker to MySQL:', err);
+                } else {
+                    console.log('Marker saved to MySQL');
+                }
+            });
+    });
+
+    socket.on('deleteMarker', () => {
+        if (!userMarkers[socket.username]) {
+            socket.emit('markerDeleteError', { success: false, message: 'No marker to delete' });
+            return;
+        }
+
+        const markerId = userMarkers[socket.username];
+        delete markers[markerId];
+        delete userMarkers[socket.username];
+
+        io.emit('removeMarker', { id: markerId });
+        pool.query('DELETE FROM markers WHERE id = ?', [markerId], (err) => {
+            if (err) {
+                console.error('Error deleting marker from MySQL:', err);
+                socket.emit('markerDeleteError', { success: false, message: 'Error deleting marker from database' });
+            } else {
+                console.log('Marker deleted from MySQL');
+                socket.emit('markerDeleted', { success: true, message: 'Marker deleted' });
+            }
+        });
+    });
+
+    socket.on('joinMarkerRoom', (markerId) => {
+        const roomId = markerId;
+        if (markers[roomId]) {
+            const currentUsersCount = roomUsers[roomId] ? roomUsers[roomId].length : 0;
+            const maxNumber = markers[roomId].max_number;
+    
+            // 사용자가 강퇴된 목록에 있는지 확인
+            if (bannedUsers[roomId] && bannedUsers[roomId].includes(socket.username)) {
+                socket.emit('banned', { success: false, message: 'You are banned from this room.' });
+                return;
+            }
+    
+            if (currentUsersCount < maxNumber) {
+                socket.join(roomId);
+                socket.room = roomId;
+                console.log(`${socket.username} joined marker room ${roomId}`);
+                
+                // 사용자 목록에 추가
+                if (!roomUsers[roomId]) {
+                    roomUsers[roomId] = [];
+                }
+                if (!roomUsers[roomId].includes(socket.username)) {
+                    roomUsers[roomId].push(socket.username);
+                }
+                socket.emit('joinedRoom', roomId, markers[roomId]);
+            } else {
+                socket.emit('roomFull', { success: false, message: 'Room is full' });
+            }
+        }
+    });
+
+    socket.on('kickUser', (data) => {
+        const { roomId, username } = data;
+
+        if (socket.username === markers[roomId].created_by) {
+            if (!bannedUsers[roomId]) {
+                bannedUsers[roomId] = [];
+            }
+            if (!bannedUsers[roomId].includes(username)) {
+                bannedUsers[roomId].push(username);
+            }
+
+            io.sockets.sockets.forEach(s => {
+                if (s.username === username && s.room === roomId) {
+                    s.leave(roomId);
+                    s.emit('kicked', { message: 'You have been kicked from the room.' });
+                    io.to(roomId).emit('updateUserLocations', {
+                        userLocations: roomUserLocations[roomId].filter(loc => loc.username !== username),
+                        users: roomUsers[roomId].filter(user => user !== username)
+                    });
+                }
+            });
+
+            roomUserLocations[roomId] = roomUserLocations[roomId].filter(loc => loc.username !== username);
+            roomUsers[roomId] = roomUsers[roomId].filter(user => user !== username);
+
+            roomUserCounts[roomId] = {
+                userCount: roomUsers[roomId].length,
+                maxNumber: markers[roomId] ? markers[roomId].max_number : 0
+            };
+
+            io.emit('updateUserCount', {
+                roomId: roomId,
+                userCount: roomUserCounts[roomId].userCount,
+                maxNumber: roomUserCounts[roomId].maxNumber
+            });
+
+            console.log(`${username} was kicked from room ${roomId} by ${socket.username}`);
+        }
+    });
+
+    socket.on('delete', (messageId) => {
+        pool.query('DELETE FROM messages WHERE id = ?', [messageId], (err) => {
+            if (err) {
+                console.error('Error deleting message from MySQL:', err);
+            } else {
+                console.log('Message deleted from MySQL');
+                io.to(socket.room).emit('delete', { messageId: messageId });
+            }
+        });
+    });
+
+    socket.on('message', (message) => {
+        io.to(message.roomId).emit('message', { text: message.text, messageId: message.messageId, username: message.username });
+
+        pool.query('INSERT INTO messages (id, username, text, roomId) VALUES (?, ?, ?, ?)', [message.messageId, message.username, message.text, message.roomId], (err) => {
+            if (err) {
+                console.error('Error saving message to MySQL:', err);
+            } else {
+                console.log('Message saved to MySQL');
+            }
+        });
+    });
+
+    socket.on('disconnect', () => {
+        console.log(`Socket disconnected: ${socket.username}`);
+        if (socket.username) {
+            pool.query('UPDATE user_login SET status = 0 WHERE username = ?', [socket.username], (err, result) => {
+                if (err) {
+                    console.error('Failed to update user status on disconnect:', err);
+                    return;
+                }
+                console.log(`Status updated to 0 for user ${socket.username}`);
+            });
+
+            const userLocations = roomUserLocations[socket.room] || [];
+            roomUserLocations[socket.room] = userLocations.filter(user => user.username !== socket.username);
+
+            const users = roomUsers[socket.room] || [];
+            roomUsers[socket.room] = users.filter(user => user !== socket.username);
+
+            io.to(socket.room).emit('updateUserLocations', {
+                userLocations: roomUserLocations[socket.room],
+                users: roomUsers[socket.room]
+            });
+
+            io.to(socket.room).emit('removeMarker', { username: socket.username });
+        }
+        if (socket.room) {
+            const roomId = socket.room;
+            roomUsers[roomId] = roomUsers[roomId].filter(user => user !== socket.username);
+    
+            roomUserCounts[roomId] = {
+                userCount: roomUsers[roomId].length,
+                maxNumber: markers[roomId] ? markers[roomId].max_number : 0
+            };
+
+            // 유저 수 업데이트 이벤트 전송
+            io.emit('updateUserCount', {
+                roomId: roomId,
+                userCount: roomUserCounts[roomId].userCount,
+                maxNumber: roomUserCounts[roomId].maxNumber
+            });}
+    });
+});
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(session({
+    secret: 'your_secret_key',
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: false }
+}));
+
+['/intro.html','/login.js', '/login.css', '/index.html', '/register.html', '/script.js', '/gps_set.js', '/style.css', '/map.html' ].forEach(file => {
+    app.get(file, (req, res) => {
+        const filePath = path.join(__dirname, file);
+        fs.readFile(filePath, (err, data) => {
+            if (err) {
+                res.status(500).send('Error loading ' + file);
+                return;
+            }
+            const contentType = {
+                '.js': 'application/javascript',
+                '.css': 'text/css',
+                '.html': 'text/html'
+            }[path.extname(file)] || 'text/plain';
+
+            res.setHeader('Content-Type', contentType);
+            res.send(data);
+        });
+    });
+});
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'intro.html'));
+});
+
+app.get('/login.html', (req, res) => {
+    if (req.session.loggedin) {
+        res.sendFile(path.join(__dirname, 'login.html'));
+    } else {
+        res.sendFile(path.join(__dirname, 'login.html'));
+    }
+});
+
+app.get('/index.html', (req, res) => {
+    if (req.session.loggedin) {
+        const indexHtml = fs.readFileSync(path.join(__dirname, 'index.html'), 'utf8');
+        const modifiedHtml = indexHtml.replace('<!--USERNAME-->', `<script>var username = "${req.session.username}";</script>`);
+        res.send(modifiedHtml);
+    } else {
+        res.redirect('/login.html');
+    }
+});
+
+app.get('/get-username', (req, res) => {
+    if (req.session.loggedin) {
+        res.json({ username: req.session.username });
+    } else {
+        res.status(401).json({ error: 'Not logged in' });
+    }
+});
+
+app.post('/signup', (req, res) => {
+    const { username, password } = req.body;
+    if (username && password) {
+        pool.execute('SELECT username FROM user_login WHERE username = ?', [username], (err, results) => {
+            if (err) {
+                res.status(500).json({ success: false, message: 'Database error' });
+                return;
+            }
+            if (results.length > 0) {
+                res.json({ success: false, message: 'Username already exists.' });
+            } else {
+                const sql = 'INSERT INTO user_login (username, password) VALUES (?, ?)';
+                pool.query(sql, [username, password], (err, result) => {
+                    if (err) {
+                        res.status(500).json({ success: false, message: 'Database error' });
+                        return;
+                    }
+                    res.json({ success: true, message: '가입이 성공되었습니다!' });
+                });
+            }
+        });
+    } else {
+        res.status(400).json({ success: false, message: 'Both username and password are required' });
+    }
+});
+
+app.post('/login', (req, res) => {
+    const { username, password } = req.body;
+
+    if (username && password) {
+        pool.execute(
+            'SELECT * FROM user_login WHERE username = ? AND password = ?',
+            [username, password],
+            (err, results) => {
+                if (err) {
+                    res.status(500).send('Database error');
+                    return;
+                }
+                if (results.length > 0) {
+                    req.session.loggedin = true;
+                    req.session.username = username;
+                    pool.query('UPDATE user_login SET status = 1 WHERE username = ?', [username], (err, result) => {
+                        if (err) {
+                            console.error('Failed to update user status:', err);
+                            return;
+                        }
+                    });
+                    res.redirect('/map.html');
+                } else {
+                    res.send('Incorrect Username and/or Password!');
+                }
+            }
+        );
+    } else {
+        res.status(400).send('Username and Password are required');
+    }
+});
+
+app.post('/getMarkers', (req, res) => {
+    const markerType = req.body.type;
+
+    pool.execute('SELECT * FROM markers WHERE type = ?', [markerType], (err, results) => {
+        if (err) {
+            console.error('Error fetching markers:', err);
+            res.status(500).send('Database error');
+            return;
+        }
+        res.json(results);
+    });
+});
+
+app.get('/getUserCounts', (req, res) => {
+    const userCounts = {};
+
+    for (const roomId in roomUsers) {
+        userCounts[roomId] = {
+            userCount: roomUsers[roomId].length,
+            maxNumber: markers[roomId] ? markers[roomId].max_number : 0
+        };
+    }
+
+    res.json(userCounts);
+});
+
+
+app.get('/map.html', (req, res) => {
+    res.sendFile(__dirname + '/map.html');
+});
+
+server.listen(8080, () => {
+    console.log('Server is listening on http://localhost:8080');
+    loadMarkersFromDB(); // 서버 시작 시 마커 로드
+});
---
+++ script.js
@@ -0,0 +1,191 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const urlParams = new URLSearchParams(window.location.search);
+    roomId = urlParams.get('roomId');  // URL에서 roomId 추출
+
+    fetch('/get-username')
+    .then(response => response.json())
+    .then(data => {
+        if (data.username) {
+            console.log("Logged in as:", data.username);
+            username = data.username; // 전역 변수로 username 설정
+            initializeChat(roomId);
+        }
+    })
+    .catch(error => console.error('Error fetching username:', error));
+});
+
+
+
+const chat = document.getElementById('chat');
+const messageInput = document.getElementById('messageInput');
+const sendButton = document.getElementById('sendButton');
+const fontSizeRange = document.getElementById('fontSizeRange');
+const userCount = document.getElementById('userCount');
+const userList = document.getElementById('userList');
+
+let users = [];
+let isAdmin = false;
+let username = '';
+
+function initializeChat(roomId) {
+    socket.emit('joinRoom', roomId);
+    console.log(`Joined room ${roomId}`);
+
+    socket.on('message', (data) => {
+        addMessageToChat(data.text, data.messageId, data.username);
+        scrollToBottom();
+    });
+
+    socket.on('delete', (data) => {
+        document.querySelectorAll(`p[data-id='${data.messageId}']`).forEach(el => el.remove());
+    });
+
+    socket.on('updateUserLocations', (data) => {
+        updateUserMarkers(data.userLocations);
+        users = data.users;
+        updateUserList();
+    });
+    socket.on('joinedRoom', (roomId, markerData, adminStatus) => {
+        isAdmin = adminStatus;
+        updateUserList();
+    });
+    socket.on('kicked', (data) => {
+        alert(data.message);
+        window.location.href = '/map.html';
+    });
+
+    socket.on('banned', (data) => {
+        if (!data.success) {
+            alert(data.message);
+        }
+    });
+}
+
+function addMessageToChat(messageText, messageId, messageUsername) {
+    const messageContainer = document.createElement('div');
+    messageContainer.classList.add('chat-message-container');
+
+    var icon = document.createElement('i');
+    icon.classList.add('fas', 'fa-trash');
+
+    const message = document.createElement('p');
+    message.dataset.id = messageId;
+    message.classList.add('chat-message');
+    message.style.fontSize = '40px';
+
+    const formattedMessageText = `${messageUsername}: ${messageText}`;
+    message.textContent = formattedMessageText;
+
+    setTextColor(message, messageText);
+
+    if (messageUsername === username) {
+        message.classList.add('self');
+        const deleteButton = document.createElement('button');
+        deleteButton.onclick = function () {
+            socket.emit('delete', message.dataset.id);
+        };
+        deleteButton.classList.add('delete-button');
+        deleteButton.appendChild(icon);
+        message.appendChild(deleteButton);
+    } else {
+        message.classList.add('other');
+    }
+
+    messageContainer.appendChild(message);
+    chat.appendChild(messageContainer);
+    if (userMarkers[messageUsername]) {
+        const popupElement = document.getElementById(`${messageUsername}-popup`).querySelector('.messages');
+        const newMessage = document.createElement('div');
+        newMessage.style.background = 'beige';
+        newMessage.style.marginBottom = '5px';
+        newMessage.textContent = messageText;
+        popupElement.appendChild(newMessage);
+
+        // 3개 이상의 메시지가 있는 경우, 가장 오래된 메시지를 서서히 제거
+        if (popupElement.children.length > 3) {
+            const oldestMessage = popupElement.children[0];
+            oldestMessage.classList.add('fade-out');
+            setTimeout(() => {
+                oldestMessage.remove();
+            }, 1000); // 애니메이션 시간 (1초)과 일치시킵니다.
+        }
+    }
+}
+
+function sendMessage() {
+    const messageId = Date.now().toString();
+    const messageText = messageInput.value.trim();
+    if (messageText) {
+        const messageData = {
+            text: messageText,
+            messageId: messageId,
+            username: username,
+            roomId: roomId
+        };
+        socket.emit('message', messageData);
+        messageInput.value = '';
+    }
+    scrollToBottom();
+}
+
+function enterkey(e) {
+    if (e.keyCode === 13) {
+        sendMessage();
+    }
+}
+
+messageInput.addEventListener('keyup', event => enterkey(event));
+
+function setTextColor(element, messageText) {
+    const firstChar = messageText.trim().charAt(0);
+    if (/[a-zA-Z]/.test(firstChar)) {
+        if (firstChar.toLowerCase() == 'a') {
+            element.classList.add('red');
+        } else {
+            element.classList.add('blue');
+        }
+    }
+}
+
+function scrollToBottom() {
+    chat.scrollTop = chat.scrollHeight;
+}
+
+function updateUserList() {
+    if (users && Array.isArray(users)) {
+        userCount.textContent = `Users: ${users.length}`;
+        userList.innerHTML = '';
+        users.forEach(user => {
+            const userItem = document.createElement('div');
+            userItem.textContent = user;
+            userItem.classList.add('user-list-item');
+
+            // 강퇴 버튼 추가
+            if (isAdmin && user !== username) { // 현재 사용자가 방 관리자일 때, 본인이 아닌 경우에만 버튼을 추가
+                const kickButton = document.createElement('button');
+                kickButton.textContent = 'Kick';
+                kickButton.classList.add('kick-button');
+                kickButton.onclick = () => kickUser(user);
+                userItem.appendChild(kickButton);
+            }
+
+            userList.appendChild(userItem);
+        });
+    } else {
+        userCount.textContent = 'Users: 0';
+        userList.innerHTML = '';
+    }
+}
+
+function kickUser(user) {
+    socket.emit('kickUser', { roomId: roomId, username: user });
+}
+
+function toggleUserList() {
+    const userListElement = document.getElementById('userList');
+    if (userListElement.style.display === 'none' || userListElement.style.display === '') {
+        userListElement.style.display = 'block';
+    } else {
+        userListElement.style.display = 'none';
+    }
+}
